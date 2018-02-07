7396b5beb9cc71666c9dda10af22c1270c36a5e9
Gordon <gburgett@watermark.org>
2018-02-06 21:53:01 -0600

    Initial gatsby starter - bootstrap-netlify
      https://github.com/konsumer/gatsby-starter-bootstrap-netlify

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Starter Blog',
+    author: 'konsumer',
+    authorLink: 'https://github.com/konsumer',
+    disqus: 'gatsby-starter-blog'// put your disqus ID here
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages'
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files'
+        ]
+      }
+    },
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass'
+  ]
+}
