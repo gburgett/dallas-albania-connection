bd0f9a4346b25882480ede787ad5a979355e0cea
Gordon <gburgett@watermark.org>
2018-02-11 22:40:00 -0600

    Fix cms styling

--- src/cms/cms.ts
+++ src/cms/cms.ts
@@ -3,15 +3,14 @@ import * as React from "react";
 import CMS from "netlify-cms";
 import "netlify-cms/dist/cms.css";
 
-//import {FooterPreview} from './preview-templates/footer'
+import {FooterPreview} from './preview-templates/footer'
 
 // Let's say you've created widget and preview components for a custom image
 // gallery widget in separate files
 //import ImageGalleryWidget from "./image-gallery-widget.js";
 //import ImageGalleryPreview from "./image-gallery-preview.js";
 
-CMS.registerPreviewStyle('/styles.css');
-//CMS.registerPreviewTemplate('footer', FooterPreview);
+CMS.registerPreviewTemplate('footer', FooterPreview);
 
 // Register the imported widget:
 //CMS.registerWidget("image-gallery", ImageGalleryWidget, ImageGalleryPreview);
