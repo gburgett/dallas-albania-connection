fc57ff37f1cbc7bd8140a6e00faf1aa3ae4ad6d5
Gordon <gburgett@watermark.org>
2018-02-11 23:30:42 -0600

    Better preview for footer

--- src/cms/preview-templates/footer.tsx
+++ src/cms/preview-templates/footer.tsx
@@ -1,9 +1,142 @@
 
 import * as React from 'react';
-//import { AboutPageTemplate } from '../../templates/about-page';
+import * as ReactDomServer from 'react-dom/server'
+import { Footer, IFooterFields } from '../../components/footer/Footer';
+import { ISitemapFields } from '../../components/footer/Sitemap';
 
-export const FooterPreview = (props) => {
-  return <pre>
-      {JSON.stringify(props, null, 2)}
-    </pre>
+export const FooterPreview = ({entry, widgetFor, widgetsFor}) => {
+  const contact = widgetsFor('contact').map(w => w.data)
+
+  const fields: IFooterFields = {
+    html: ReactDomServer.renderToStaticMarkup(widgetFor('body')) as string,
+    frontmatter: {
+      contact: contact,
+      mailchimp: entry.getIn(['data', 'mailchimp'])
+    }
+  }
+
+  return <Footer sitemap={fakeSitemap} fields={fields} />
 };
+
+
+const fakeSitemap: ISitemapFields = {
+  pages: [
+    { path: '/about', title: 'About' },
+    { path: '/fake', title: 'Fake Page'}
+  ],
+  posts: [
+    { path: '/fake1', title: 'Fake Post 1' },
+    { path: '/fake2', title: 'Fake Post 2' },
+    { path: '/fake3', title: 'Fake Post 3' },
+    { path: '/fake4', title: 'Fake Post 4' },
+    { path: '/fake5', title: 'Fake Post 5' },
+  ]
+}
+
+/*
+{
+  "collection": {
+    "name": "footer",
+    "label": "Site Footer",
+    "folder": "src/components/footer",
+    "create": false,
+    "fields": [
+      {
+        "label": "Contact Info",
+        "name": "contact",
+        "widget": "list",
+        "fields": [
+          {
+            "label": "Name",
+            "name": "name",
+            "widget": "string",
+            "required": true
+          },
+          {
+            "label": "Email",
+            "name": "email",
+            "widget": "string",
+            "required": true
+          },
+          {
+            "label": "Phone",
+            "name": "phone",
+            "widget": "string"
+          }
+        ]
+      },
+      {
+        "label": "Mailchimp Signup List Url",
+        "name": "mailchimp",
+        "widget": "string"
+      },
+      {
+        "label": "Body",
+        "name": "body",
+        "widget": "markdown"
+      }
+    ],
+    "type": "folder_based_collection"
+  },
+  "entry": {
+    "partial": false,
+    "path": "src/components/footer/Footer.md",
+    "isModification": null,
+    "raw": "---\ncontact:\n  - name: Gordon Burgett\n    email: info@albania-dallas-connection.netlify.com\n    phone: 972-275-9023\nmailchimp: https://gordonburgett.us11.list-manage.com/subscribe/post?u=fbcbfba66020e12dd41b9cf1b&amp;id=4a0067c925\n---\n\nCopyright © 2018 Gordon Burgett\nAll rights reserved",
+    "data": {
+      "contact": [
+        {
+          "name": "Gordon Burgett",
+          "email": "info@albania-dallas-connection.netlify.com",
+          "phone": "972-275-9023"
+        }
+      ],
+      "mailchimp": "https://gordonburgett.us11.list-manage.com/subscribe/post?u=fbcbfba66020e12dd41b9cf1b&amp;id=4a0067c925",
+      "body": "\nCopyright © 2018 Gordon Burgett\nAll rights reserved"
+    },
+    "slug": "Footer",
+    "metaData": null,
+    "newRecord": false,
+    "isFetching": false,
+    "label": null,
+    "collection": "footer"
+  },
+  "fields": [
+    {
+      "label": "Contact Info",
+      "name": "contact",
+      "widget": "list",
+      "fields": [
+        {
+          "label": "Name",
+          "name": "name",
+          "widget": "string",
+          "required": true
+        },
+        {
+          "label": "Email",
+          "name": "email",
+          "widget": "string",
+          "required": true
+        },
+        {
+          "label": "Phone",
+          "name": "phone",
+          "widget": "string"
+        }
+      ]
+    },
+    {
+      "label": "Mailchimp Signup List Url",
+      "name": "mailchimp",
+      "widget": "string"
+    },
+    {
+      "label": "Body",
+      "name": "body",
+      "widget": "markdown"
+    }
+  ],
+  "fieldsMetaData": {}
+}
+*/
