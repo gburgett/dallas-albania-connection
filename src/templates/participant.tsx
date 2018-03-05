b239cea5bd566a453282fd089caa546489b1490a
Gordon <gburgett@watermark.org>
2018-03-05 17:37:55 -0600

    Add participant-only page

---
+++ src/templates/participant.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react'
+import { Row, Col, Container, Card, CardTitle, CardGroup, CardBody } from 'reactstrap'
+import Helmet from 'react-helmet'
+import * as graphql from 'graphql'
+import { basename } from 'path'
+import Link from 'gatsby-link'
+
+import Hero from '../components/hero/Hero'
+
+export default function Template ({ data }: IPageContext<ITemplateData>) {
+  const { markdownRemark: post } = data
+  const { heroimage, title } = post.frontmatter;
+  const {siteUrl} = data.site.siteMetadata;
+
+  return (
+    <div>
+      <Helmet title={post.frontmatter.title}>
+        {heroimage && <meta property="og:image" content={siteUrl + heroimage}></meta>}
+      </Helmet>
+
+      <Container fluid>
+        {heroimage && <Hero image={heroimage} />}
+      </Container>
+
+      <Container>
+
+        <h1 className='display-3'>{title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </Container>
+    </div>
+  )
+}
+
+export interface ITemplateData {
+  site: ISite,
+  markdownRemark: {
+    html: string,
+    frontmatter: {
+      path: string,
+      date: string,
+      title: string,
+      heroimage: string
+    }
+  }
+}
+
+export const pageQuery = graphql`
+  query PageByPath($path: String!) {
+    site {
+      siteMetadata {
+        siteUrl
+      }
+    }
+
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        path
+        title
+        heroimage
+      }
+    }
+  }
+`
