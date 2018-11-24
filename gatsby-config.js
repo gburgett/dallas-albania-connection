d76ddabc0110302a7d8170131c07ec4fc99e68ba
Gordon <gburgett@watermark.org>
2018-11-24 15:50:11 -0600

    Upgrade netlify-cms
      still facing an error with importing styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
-        modulePath: `${__dirname}/src/cms/cms.ts`
+        modulePath: `${__dirname}/src/cms/cms`
       }
     },
     {
