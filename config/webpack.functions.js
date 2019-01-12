7e1fcaafa02d2cbcec834fbfa4c286c44b5e113a
Gordon <gburgett@watermark.org>
2019-01-12 16:00:42 -0600

    Use typescript for lambda functions

---
+++ config/webpack.functions.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: "development",
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: "ts-loader" }
+    ]
+  }
+}
