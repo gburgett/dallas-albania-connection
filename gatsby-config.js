b99d55cee3bfc3f16b0d91a99af5634d829d5d56
Gordon <gburgett@watermark.org>
2019-03-17 11:12:49 -0500

    Remove gatsby-remark-prismjs
      We don't need code highlighting, there's no code on this blog.

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,6 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
-          'gatsby-remark-prismjs',
           'gatsby-remark-copy-linked-files'
         ]
       }
