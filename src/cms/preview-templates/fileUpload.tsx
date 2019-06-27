69d1e586d54cf2712e81bdf6161694d9d99d73d7
Gordon <gburgett@watermark.org>
2019-06-27 13:37:58 +0200

    Update CMS

--- src/cms/preview-templates/fileUpload.tsx
+++ src/cms/preview-templates/fileUpload.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 
 export const FileUploadPreview = ({entry, getAsset}) => {
+  if (!entry) {
+    return <a><em>Cannot load file entry</em></a>
+  }
+
   const dataUrl = entry.get(['data', 'url'])
   const linkText = entry.get(['data', 'text'])
 
